9795a62ed1b2a47682694d76b3d4bbdd6f7abbed
marcosborg <m.borges.mail@gmail.com>
2023-10-17 17:57:45 +0100

    Update

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -74,7 +74,7 @@ class WebsiteController extends Controller
 
         $car = Http::get('https://mundotvde.pt/api/stand-car/' . $request->car_id)->json();
 
-        Http::post('http://127.0.0.1:8000/api/contact', [
+        Http::post('https://mundotvde.pt/api/contact', [
             'name' => $request->name,
             'car' => $car['brand']['name'] . ' ' . $car['car_model']['name'],
             'email' => $request->email,
